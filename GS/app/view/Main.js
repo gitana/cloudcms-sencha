86580b9ac2ef69ded0b9224b73f373500d43cd7a
drq <quyong2000@yahoo.com>
2012-08-15 21:46:54 -0400

    Initial check in with Hello World example.

---
+++ GS/app/view/Main.js
@@ -0,0 +1,68 @@
+Ext.define("GS.view.Main", {
+    extend: 'Ext.tab.Panel',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+
+        listeners: {
+            activate: 'onActivate'
+        },
+
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'Welcome',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Welcome to Sencha Touch 2'
+                },
+
+                html: [
+                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
+                    "and refresh to change what's rendered here."
+                ].join("")
+            },
+            {
+                title: 'Get Started',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Getting Started'
+                    },
+                    {
+                        xtype: 'video',
+                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
+                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                    }
+                ]
+            }
+        ]
+    },
+
+    onActivate: function(me, container) {
+        new Gitana({
+            "clientId" : "676e3450-6131-46c2-99cc-496aa2ad80fa",
+            "clientSecret" : "5fGkvesH/tWEMX6SpevL54rY6iJK5ADzLH963sif2ljrWvFOhV2zXv6rSpLF2uMWlJ9SG0uEO9uQO4JZac0i7DZquA/5W8ixJwhj76g0Ksk="
+        }).authenticate({
+            "username" : "demo",
+            "password" : "demo"
+        }).then(function() {
+            var authInfo = this.getDriver().getAuthInfo();
+            var name = authInfo.getPrincipalName();
+            me.innerItems[0].setHtml('Hello ' + name + '! Welcome to Cloud CMS!');
+        });
+    }
+});
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-cloudcms-sencha
-===============
+## Welcome to Cloud CMS Sencha Touch Project ##
 
-Cloud CMS Sencha Touch Integration
+This project provides reference applications which demonstrate how to build a Sencha Touch application
+on top of Cloud CMS platform. It is intended to help Sencha Touch developers to get started with developing
+mobile projects with Cloud CMS platform as their content platform.
+
+* Hello World: A simple application that shows how to make connection to Cloud CMS platform.
+
+### Resources
+
+* Cloud CMS: http://www.cloudcms.com
+* Git Hub: http://github.com/gitana/cloudcms-sencha
+* JavaScript Driver: http://code.cloudcms.com/javascript-driver
+* JavaScript Driver Documentation: http://docs.cloudcms.com
+* JavaScript Driver Samples: http://code.cloudcms.com/javascript-samples/#/
+* Getting Started Guide: http://www.cloudcms.com/tour/resources/developers/sencha
+
+### Questions and Bug Reports
+
+http://github.com/gitana/cloudcms-sencha
+
+### Maintainers
+
+* Yong Qu     drq@cloudcms.com
+* Michael Uzquiano     uzi@cloudcms.com
